2ba415dc204e3bdfe7ab6a1e27b866fad10c368c
DanielPerezGonz <116338863+DanielPerezGonz@users.noreply.github.com>
2023-02-03 13:58:34 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+const http = require("http");
+const fs = require("fs");
+
+http.createServer(function(request, response){
+	fs.readDile("index.html", function(err, data){
+		if (err){
+			console.error(err);
+			return;
+		}
+		
+		response.writeHead(200, {"Content-Type":"text/html"});
+		response.write(data);
+	
+		response.end();
+	});
+	
+})listen(6969);
