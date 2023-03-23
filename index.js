d4eb838b85f7874ddcd777657ccf13cd3591d9aa
DanielPerezGonz <116338863+DanielPerezGonz@users.noreply.github.com>
2023-03-23 19:38:56 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
-const http = require("httgap");
+
+
+
+const http = require("http");
 const fs = require("fs");
 
-let files = new files_static.server("./public");
 
-function send_index(response){
+function send_index (response)
+{
 	fs.readFile("index.html", function(err, data){
 		if (err){
 			console.error(err);
 			return;
 		}
-		
+
 		response.writeHead(200, {"Content-Type":"text/html"});
 		response.write(data);
-	
+
 		response.end();
 	});
 }
 
-function send_player(response){
+function send_player (response)
+{
 	fs.readFile("player.png", function(err, data){
 		if (err){
 			console.error(err);
 			return;
 		}
-		
+
 		response.writeHead(200, {"Content-Type":"image/png"});
 		response.write(data);
-	
+
 		response.end();
 	});
 }
 
+
 http.createServer(function(request, response){
-	
+	console.log(request.url);
+
 	let url = request.url.split("/");
-	
+
 	switch (url[1]){
 		case "player.png":
-			send_player(response)
+			send_player(response);
+
 			break;
-		
+
 		default:
 			send_index(response);
-		
 	}
-	
-	
-	
-})listen(6969);
+}).listen(6969);
